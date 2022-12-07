b6f3045bc6195832dadbd3da37ed0f3e9de20011
henrique2003 <henrique.de.melo.cristioglu@gmail.com>
2022-12-07 17:27:42 -0300

    feat: add register user

---
+++ src/app/controllers/user.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express'
+import bcrypt from 'bcrypt'
+
+import prismaClient from '../prisma'
+
+export default class User {
+  public async register (req: Request, res: Response): Promise<Response> {
+    try {
+      const { email, password } = req.body
+
+      const encriptedPassword = await bcrypt.hash(password, 10)
+
+      const newUser = await prismaClient.user.create({
+        data: {
+          email,
+          password: encriptedPassword
+        }
+      })
+
+      delete newUser.password
+
+      return res.status(201).json(newUser)
+    } catch (error) {
+      return res.status(500).json({ error })
+    }
+  }
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express'
 
 import Products from './controllers/product'
+import User from './controllers/user'
 
 const routes = Router()
 
 const products = new Products()
+const user = new User()
 
 // Products
 routes.get('/products', products.getProducts)
+
+// User
+routes.post('/user', user.register)
 
 export default routes
